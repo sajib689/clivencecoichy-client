fcb199f1fc5d7e52f6381b9fa6de8ed229ec7bbf
Sadik Ahmmed Tonmoy <Sadikahmed2258@gmail.com>
2024-11-30 13:14:32 +0600

    props update

--- src/app/(common)/blog/page.tsx
+++ src/app/(common)/blog/page.tsx
@@ -1,5 +1,5 @@
+"use server"
 /* eslint-disable @typescript-eslint/no-explicit-any */
-// app/blog/page.tsx (Server Component)
 import React from "react";
 import bgImage from "@/assets/trustedHuman.png";
 import SmallBanner from "@/components/shared/SmallBanner/SmallBanner";
@@ -7,15 +7,16 @@ import AllBlogsGrid from "@/components/pages/allBlogs/allBlogsGrid/allBlogsGrid"
 import TrustedTrader from "@/components/shared/TrustedTrader/TrustedTrader";
 import ImproveProperty from "@/components/shared/ImproveProperty/ImproveProperty";
 import ScrollToTop from "@/components/ui/ScrollToTop";
+const baseUrl = process.env.BASE_URL
 
 const BlogsPage = async ({ searchParams }: any) => {
   const searchItems = await searchParams;
-  const page = await (parseInt(searchItems?.page) || 1);
+  const page = (parseInt(searchItems?.page) || 1);
   const limit = 10;
 
   // Fetch blog data server-side
   const res = await fetch(
-    `${process.env.BASE_URL}/blog?page=${page}&limit=${limit}`,
+    `${baseUrl}/blog?page=${page}&limit=${limit}`,
     {
       cache: "no-store", // Avoid caching to get fresh data on each request
     }
