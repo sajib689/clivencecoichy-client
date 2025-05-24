c2246ad911e6193ea9e0221bbae28585ae9ccd86
anarul <ai.anarul4232@gmail.com>
2025-05-25 03:47:26 +0600

    window page created and banner section added

---
+++ src/app/(common)/window/page.tsx
@@ -0,0 +1,12 @@
+import WindowBanner from "@/components/pages/window/WindowBanner";
+import React from "react";
+
+const WindowsPage = () => {
+  return (
+    <div>
+      <WindowBanner />
+    </div>
+  );
+};
+
+export default WindowsPage;
