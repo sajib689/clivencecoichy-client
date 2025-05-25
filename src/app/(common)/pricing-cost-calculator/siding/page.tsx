fbceebbd43eab6aa207c86a3406429bbbdcd4389
anarul <ai.anarul4232@gmail.com>
2025-05-26 03:55:36 +0600

    Calculator siding page added

---
+++ src/app/(common)/pricing-cost-calculator/siding/page.tsx
@@ -0,0 +1,14 @@
+import SidingCalculatorBanner from "@/components/pages/pricing-cost-calculator/siding/SidingCalculatorBanner";
+import SidingCalculatorSection from "@/components/pages/pricing-cost-calculator/siding/SidingCalculatorSection";
+import React from "react";
+
+const PricingCalculatorSidingPage = () => {
+  return (
+    <div>
+      <SidingCalculatorBanner />
+      <SidingCalculatorSection />
+    </div>
+  );
+};
+
+export default PricingCalculatorSidingPage;
