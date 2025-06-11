0685bddda455188a8a18ce9a6b93373b436812e5
anarul <ai.anarul4232@gmail.com>
2025-06-12 02:35:31 +0600

    refactor(middleware): remove token validation and redirection logic

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,37 +1,9 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import { jwtDecode } from "jwt-decode";
 
 export async function middleware(request: NextRequest) {
   const token = request.cookies.get("accessToken")?.value;
   console.log("Token from cookies: ", token);
-
-  // Redirect to login if token is not present
-  if (!token) {
-    const loginUrl = new URL("/login", request.url);
-    return NextResponse.redirect(loginUrl);
-  }
-
-  try {
-    // Decode the token
-    const decodedToken: any = jwtDecode(token);
-    // console.log("Decoded Token: ", decodedToken);
-
-    // Ensure the token has a valid email and role
-    if (!decodedToken.email) {
-      //   console.log("Token missing email or role");
-      const loginUrl = new URL("/login", request.url);
-      return NextResponse.redirect(loginUrl);
-    }
-
-    // Proceed to the requested route
-    return NextResponse.next();
-  } catch (error) {
-    console.error("Invalid token or error during decoding:", error);
-    const loginUrl = new URL("/login", request.url);
-    return NextResponse.redirect(loginUrl);
-  }
 }
 
 export const config = {
